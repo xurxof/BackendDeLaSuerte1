76fe53a816d3e2c69e3943b6d722f2af96c811b5
xurxof <xurxof@gmail.com>
2022-04-02 14:00:40 -0400

    reto 2: funciona!

--- BackedReto1/src/App.svelte
+++ BackedReto1/src/App.svelte
@@ -1,5 +1,11 @@
 
 <script> 
+	let count = 0;
+	let gameId = 0;
+	let goal = 0;
+
+	let emoji = '';
+
 	async function getEmojiItems() {
 		var myHeaders = new Headers();
 		
@@ -15,18 +21,77 @@
 
 		/* Return result if successful
 		*/
-		if(response.ok) {
-			console.log("OK");
+		if(response.ok) { 
 			return result;
 		} 
 		else {
-			console.log("error");  
+			console.log("error getEmojiItems");  
 		}
 	}
    
+	async function getNewMiningGame() {
+		var requestOptions = {
+			method: 'POST', 
+			redirect: 'follow'
+			};
 
+		const response = await fetch("https://w4390n0i.directus.app/items/Games", requestOptions)
+
+		const result = response.json();
+
+		if(response.ok) {
+			return result;
+		} 
+		else {
+			console.log("error getNewMiningGame");  
+		}
+
+	}
+
+	function incrementCount2() {
+		
+		if (count==goal) {
+			return;
+		}
+		console.log("incrementCount2",gameId);
+		var requestOptions = {
+			method: 'PATCH', 
+			redirect: 'follow',
+			body: JSON.stringify({
+				current: count+1,
+			  }),
+			  headers: {
+				  	'Content-type': 'application/json; charset=UTF-8',
+			  },
+			};
+		const response = fetch("https://w4390n0i.directus.app/items/Games/"+gameId, requestOptions)
+		.then((response) => response.json());
+		
+		count = count  +1 ; 
+		if (count==goal) {
+			emoji = "👑";
+		}
+	}
+
+
+	function incrementCount() {
+		count += 1;
+	}
+
+	function setGameValues (gameIdP, goalP) {
+		gameId = gameIdP;
+		goal = goalP;
+		console.log("setValues",gameId,goal);
+		return '';
+	}
+
+	function setEmojiValues (emojiP) {
+		emoji = emojiP;
+		return '';
+	}
 	const promise = getEmojiItems();
  
+	const promiseMiningGame = getNewMiningGame();
  
 	import { fade, fly } from 'svelte/transition';
 </script>
@@ -35,18 +100,30 @@
 	<title>Home</title>
 </svelte:head>
 <main>
+ 
+
+	{#await promiseMiningGame then game}
+		{ setGameValues (game.data.id, game.data.goal)}
+	{/await}
 	{#await promise}
  		<p>Loading...</p>
 	{:then items} 
-		<h1 in:fly="{{ y: 200, duration: 2000 }}">{items.data[Math.floor(Math.random()*items.data.length)].Emoji}</h1> 
+		<h1 in:fade="{{ y: 200, duration: 2000 }}">{emoji}</h1> 
+		{setEmojiValues (items.data[Math.floor(Math.random()*items.data.length)].Emoji)}
 	{:catch error}
 		<p style="color: red">{error.message}</p>
 	{/await}
 
+	
 
+	
+<button on:click={incrementCount2}>
+	⛏️
+</button>
  
 	<div class="footer"> 
-		<p> Solucion para Reto #1 de <a href="https://github.com/malandrinersdev/backend-de-la-suerte"> Backend de la suerte</a> | Visita la comunidad <a href="https://danielprimo.io">DanielPrimo.io</a> para otros retos en pantuflas como este | Done with ❤️ </p></div>
+		<p> Solucion para Reto de <a href="https://github.com/malandrinersdev/backend-de-la-suerte"> Backend de la suerte</a> | Visita la comunidad <a href="https://danielprimo.io">DanielPrimo.io</a> para otros retos en pantuflas como este | Done with ❤️ </p>
+	</div>
   
 </main>
 
