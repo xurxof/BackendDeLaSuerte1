876961ee833b9987d34b418d059dd7a10bb6cce1
xurxof <xurxof@gmail.com>
2022-04-03 20:05:32 +0200

    format App.svelte

--- BackedReto1/src/App.svelte
+++ BackedReto1/src/App.svelte
@@ -1,130 +1,134 @@
-
-<script> 
+<script>
 	let count = 0;
 	let gameId = 0;
 	let goal = 0;
 
-	let emoji = '';
+	let emoji = "";
 
 	async function getEmojiItems() {
 		var myHeaders = new Headers();
-		
-		var requestOptions = {
-			method: 'GET', 
-			redirect: 'follow'
-			};
 
-			
-		const response = await fetch("https://w4390n0i.directus.app/items/Emojis", requestOptions)
+		var requestOptions = {
+			method: "GET",
+			redirect: "follow",
+		};
+
+		const response = await fetch(
+			"https://w4390n0i.directus.app/items/Emojis",
+			requestOptions
+		);
 		console.log(response);
-		const result = response.json()
+		const result = response.json();
 
 		/* Return result if successful
-		*/
-		if(response.ok) { 
+		 */
+		if (response.ok) {
 			return result;
-		} 
-		else {
-			console.log("error getEmojiItems");  
+		} else {
+			console.log("error getEmojiItems");
 		}
 	}
-   
+
 	async function getNewMiningGame() {
 		var requestOptions = {
-			method: 'POST', 
-			redirect: 'follow'
-			};
+			method: "POST",
+			redirect: "follow",
+		};
 
-		const response = await fetch("https://w4390n0i.directus.app/items/Games", requestOptions)
+		const response = await fetch(
+			"https://w4390n0i.directus.app/items/Games",
+			requestOptions
+		);
 
 		const result = response.json();
 
-		if(response.ok) {
+		if (response.ok) {
 			return result;
-		} 
-		else {
-			console.log("error getNewMiningGame");  
+		} else {
+			console.log("error getNewMiningGame");
 		}
-
 	}
 
 	function incrementCount2() {
-		
-		if (count==goal) {
+		if (count == goal) {
 			return;
 		}
-		console.log("incrementCount2",gameId);
+		console.log("incrementCount2", gameId);
 		var requestOptions = {
-			method: 'PATCH', 
-			redirect: 'follow',
+			method: "PATCH",
+			redirect: "follow",
 			body: JSON.stringify({
-				current: count+1,
-			  }),
-			  headers: {
-				  	'Content-type': 'application/json; charset=UTF-8',
-			  },
-			};
-		const response = fetch("https://w4390n0i.directus.app/items/Games/"+gameId, requestOptions)
-		.then((response) => response.json());
-		
-		count = count  +1 ; 
-		if (count==goal) {
+				current: count + 1,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		};
+		const response = fetch(
+			"https://w4390n0i.directus.app/items/Games/" + gameId,
+			requestOptions
+		).then((response) => response.json());
+
+		count = count + 1;
+		if (count == goal) {
 			emoji = "👑";
 		}
 	}
-
 
 	function incrementCount() {
 		count += 1;
 	}
 
-	function setGameValues (gameIdP, goalP) {
+	function setGameValues(gameIdP, goalP) {
 		gameId = gameIdP;
 		goal = goalP;
-		console.log("setValues",gameId,goal);
-		return '';
+		console.log("setValues", gameId, goal);
+		return "";
 	}
 
-	function setEmojiValues (emojiP) {
+	function setEmojiValues(emojiP) {
 		emoji = emojiP;
-		return '';
+		return "";
 	}
 	const promise = getEmojiItems();
- 
+
 	const promiseMiningGame = getNewMiningGame();
- 
-	import { fade, fly } from 'svelte/transition';
+
+	import { fade, fly } from "svelte/transition";
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 <main>
- 
-
 	{#await promiseMiningGame then game}
-		{ setGameValues (game.data.id, game.data.goal)}
+		{setGameValues(game.data.id, game.data.goal)}
 	{/await}
 	{#await promise}
- 		<p>Loading...</p>
-	{:then items} 
-		<h1 in:fade="{{ y: 200, duration: 2000 }}">{emoji}</h1> 
-		{setEmojiValues (items.data[Math.floor(Math.random()*items.data.length)].Emoji)}
+		<p>Loading...</p>
+	{:then items}
+		<h1 in:fade={{ y: 200, duration: 2000 }}>{emoji}</h1>
+		{setEmojiValues(
+			items.data[Math.floor(Math.random() * items.data.length)].Emoji
+		)}
 	{:catch error}
 		<p style="color: red">{error.message}</p>
 	{/await}
 
-	
+	<button on:click={incrementCount2}> ⛏️ </button>
 
-	
-<button on:click={incrementCount2}>
-	⛏️
-</button>
- 
-	<div class="footer"> 
-		<p> Solucion para Reto de <a href="https://github.com/malandrinersdev/backend-de-la-suerte"> Backend de la suerte</a> | Visita la comunidad <a href="https://danielprimo.io">DanielPrimo.io</a> para otros retos en pantuflas como este | Done with ❤️ </p>
+	<div class="footer">
+		<p>
+			Solucion para Reto de <a
+				href="https://github.com/malandrinersdev/backend-de-la-suerte"
+			>
+				Backend de la suerte</a
+			>
+			| Visita la comunidad
+			<a href="https://danielprimo.io">DanielPrimo.io</a> para otros retos
+			en pantuflas como este | Done with ❤️
+		</p>
 	</div>
-  
 </main>
 
 <style>
@@ -141,18 +145,18 @@
 		font-size: 8em;
 		font-weight: 200;
 	}
- 
+
 	p {
-		font-size: .8em;
+		font-size: 0.8em;
 	}
-	.footer {  
-		position: fixed;  
-		left: 10px;  
-		bottom: 5px;  
-		right: 10px;   
-		width: 95%;    
-		text-align: center;  
-	} 
+	.footer {
+		position: fixed;
+		left: 10px;
+		bottom: 5px;
+		right: 10px;
+		width: 95%;
+		text-align: center;
+	}
 
 	@media (min-width: 640px) {
 		main {
